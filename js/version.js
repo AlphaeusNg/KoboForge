ed650659b6f3651acbc743667fadcdbd37d02e76
Alphaeus Ng <78588510+AlphaeusNg@users.noreply.github.com>
2026-08-28 13:53:51 +0800

    Keep the phone image-size slider from scrolling the page

--- js/version.js
+++ js/version.js
@@ -6,7 +6,7 @@
   "use strict";
 
   global.SITE_VERSION = {
-    id: "2026.08.25.8",
+    id: "2026.08.28.1",
     repo: "KoboForge",
     label: "Kobo EPUB converter",
     asset: function (path) {
